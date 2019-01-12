09bcd180875825da5a608f3f1abd04e0ef997c4a
Eric Tillberg <eric.tillberg@teecom.com>
2019-01-11 19:17:44 -0500

    Remove some default boilerplate

---
+++ src/GoalSetter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GoalSetter extends Component {
+  render() {
+    return (
+      <h1 id='root'>Goal Setter</h1>
+    );
+  }
+}
+
+export default GoalSetter;

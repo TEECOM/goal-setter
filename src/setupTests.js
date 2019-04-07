efc7819da0e1c56897d600a096523fa715ae01b7
Eric Tillberg <eric.tillberg@gmail.com>
2019-04-07 18:36:53 -0400

    Add sinon

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as sinon from 'sinon';
+
+afterEach(() => {
+  sinon.restore();
+});
+
 
 configure({ adapter: new Adapter() });
